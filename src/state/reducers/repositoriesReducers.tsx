51b97232c520e36ed5de9ceaca6cd01e9dba929c
David Moll <david.leander.moll@gmail.com>
2022-01-17 13:25:37 +0100

    29: Adding Interfaces and Enums

--- src/state/reducers/repositoriesReducers.tsx
+++ src/state/reducers/repositoriesReducers.tsx
@@ -4,15 +4,43 @@ interface RepositoriesState {
 	data: string[];
 }
 
-const reducer = (state: RepositoriesState, action: any): RepositoriesState => {
+interface SearchRepositoriesAction {
+	type: ActionType.SEARCHREPOSITORIES;
+}
+
+interface SearchRepositoriesSuccessAction {
+	type: ActionType.SEARCHREPOSITORIESSUCCESS;
+	payload: string[];
+}
+
+interface SearchRepositoriesErrorAction {
+	type: ActionType.SEARCHREPOSITORIESERROR;
+	payload: string;
+}
+
+type Action =
+	| SearchRepositoriesAction
+	| SearchRepositoriesSuccessAction
+	| SearchRepositoriesErrorAction;
+
+enum ActionType {
+	SEARCHREPOSITORIES = "search_repositories",
+	SEARCHREPOSITORIESSUCCESS = "search_repositories_success",
+	SEARCHREPOSITORIESERROR = "search_repositories_error",
+}
+
+const reducer = (
+	state: RepositoriesState,
+	action: Action
+): RepositoriesState => {
 	switch (action.type) {
-		case "search_repositories":
+		case ActionType.SEARCHREPOSITORIES:
 			//Since reducer's return value is of type RepositoriesState,
 			//data has to be an array. Entering a number will not work anymore
 			return { loading: true, error: null, data: [] };
-		case "search_repositories_success":
+		case ActionType.SEARCHREPOSITORIESSUCCESS:
 			return { loading: false, error: null, data: action.payload };
-		case "search_repositories_error":
+		case ActionType.SEARCHREPOSITORIESERROR:
 			return { loading: false, error: action.payload, data: [] };
 		default:
 			return state;
